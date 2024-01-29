2b1658abd019e69d8ebe350cedad2d7a63619960
wheakerd <wheakerd@qq.com>
2024-01-29 16:31:55 +0800

    init

---
+++ src/CoroutineServer.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Skernl\ServerManager;
+
+/**
+ * @CoroutineServer
+ * @\Skernl\ServerManager\CoroutineServer
+ */
+class CoroutineServer
+{
+}
---
+++ src/ServerConfig.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Skernl\ServerManager;
+
+/**
+ * @ServerConfig
+ * @\Skernl\Server\ServerConfig
+ */
+class ServerConfig
+{
+    public function __construct()
+    {
+    }
+
+    public function a()
+    {
+
+    }
+}
